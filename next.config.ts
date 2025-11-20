a0905fd178bd1fbc44a56c999b3a8e9d7976d9da
Ronai Daniel <daniel.ronai.mail@gmail.com>
2025-11-20 19:31:29 +0100

    implement full vapi functionality (without transcript)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+      remotePatterns: [
+          { hostname: 'img.clerk.com'}
+      ]
+  }
 };
 
 export default nextConfig;
